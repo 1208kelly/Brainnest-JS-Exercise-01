b461c57d78a6261b5b9b288241d4a06fe61fc09f
Bob Kelso <49724332+1208kelly@users.noreply.github.com>
2023-02-05 17:29:55 +0000

    Added more js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const para = document.createElement("p");
-const node = document.createTextNode("Hey I'm red!");
+const p1 = document.createElement("p");
+const p2 = document.createElement("p");
+const div = document.createElement("div");
+const h1 = document.createElement("h1");
+const h3 = document.createTextNode("h3");
+
+
+document.body.appendChild(p1)
+document.body.appendChild(div)
+document.body.appendChild(h3)
+
+div.body.appendChild(p2)
+div.body.appendChild(h1)
